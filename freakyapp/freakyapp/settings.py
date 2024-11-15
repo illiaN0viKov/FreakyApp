317e199e9d692f5582c73f073c7afa11a36119e3
Illia Novikov <166924986+illiaN0viKov@users.noreply.github.com>
2024-11-15 14:51:20 +0000

    Added Templates and Static

--- freakyapp/freakyapp/settings.py
+++ freakyapp/freakyapp/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = "freakyapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR/'templates', 
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -102,8 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
 
 LANGUAGE_CODE = "en-us"
 
@@ -114,12 +115,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   BASE_DIR/'static',  
+]
+
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
