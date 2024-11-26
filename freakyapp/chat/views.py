e8b0f166c3c3ac95e30c73b59f2f5f48b3ad7714
illiaN0viKov <illia.novikov3hunna@gmail.com>
2024-11-26 13:00:11 +0100

    chat keep working

--- freakyapp/chat/views.py
+++ freakyapp/chat/views.py
@@ -10,14 +10,19 @@ def chat_view(request):
     chat_group = get_object_or_404(ChatGroup, chat_name = "TYPE SHIT")
     chat_messages = chat_group.chat_messages.all()[:30]
     form = MessageForm()
-
-    if request.method == "POST":
+    
+    if request.headers.get('HX-Request'):
         form = MessageForm(request.POST)
         if form.is_valid:
             message = form.save(commit=False)
             message.author = request.user
             message.group = chat_group
             message.save()
-            return redirect('chat')
+            context = {
+                "message": message,
+                "user": request.user,
+
+            }
+            return render(request, 'chat/partials/chat_message_p.html', context)
 
     return render(request, 'chat/chat.html', {"chat_messages":chat_messages, "form": form})
