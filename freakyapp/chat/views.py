a3e4d5338e1170addcc5bdd92c10bbdef1e19a8b
illiaN0viKov <illia.novikov3hunna@gmail.com>
2024-12-16 18:10:16 +0100

    only event users in chat

--- freakyapp/chat/views.py
+++ freakyapp/chat/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from .models import ChatGroup, GroupMessage
 from .forms import *
 
 
 @login_required
-def chat_view(request, chat_name):
+def chat_view(request, chat_name): 
     chat_group = get_object_or_404(ChatGroup, chat_name = chat_name)
+    if request.user != chat_group.event.host and not chat_group.event.is_user_joined(request.user):
+        return HttpResponseForbidden("You do not have permission to access this chat.")
     chat_messages = chat_group.chat_messages.all()[:30]
     form = MessageForm()
     
